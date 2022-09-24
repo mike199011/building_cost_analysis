4f3968f093d228a271d0d8a1ee02afede7425bd4
Michael Astl <astl.pma@outlook.de>
2022-09-24 07:55:43 +0200

    extend all_cost jupyter script

--- src/building_cost_analysis/settings.py
+++ src/building_cost_analysis/settings.py
@@ -1,4 +1,4 @@
 PATH_GEWERKE = r'C:\Users\Michael\Google Drive\Projekt_Palast\Finanzen\Kostenberechung\Kostenberechnung_Gewerke (1).xlsx'
 DIR_ANGEBOTE = r'C:\Users\Michael\Google Drive\Projekt_Palast\Finanzen\Kostenberechung\Analyse'
 PATH_AUSGABEN = r'C:\Users\Michael\Google Drive\Projekt_Palast\Finanzen\Ausgaben_Bauprojekt Astl_2021_2022.xlsx'
-PATH_KOSTENUEBERSICHT = r'C:\Users\Michael\Google Drive\Projekt_Palast\Finanzen\Kostenberechnung_Uebersicht.xlsx'
+PATH_KOSTENUEBERSICHT = r'C:\Users\Michael\Google Drive\Projekt_Palast\Finanzen\Kostenberechung\Kostenberechnung_Uebersicht.xlsx'
